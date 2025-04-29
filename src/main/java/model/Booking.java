0fd6716816414d3a893fe034f2aee1a7185c3d76
DilniRupasinghe <dilni.20230180@iit.ac.lk>
2025-04-29 19:39:39 +0530

    booking model updated

--- src/main/java/model/Booking.java
+++ src/main/java/model/Booking.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document
 public class Booking {
+    @Id
+    private String id;
+    private String customerName;
+    private String address;
+    private LocalDateTime dateTime;
+    private String serviceId;
+    private String userId;
+    
 }
