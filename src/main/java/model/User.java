aecf4168cf640203ed858b22f4abf86fd23fa1c8
DilniRupasinghe <dilni.20230180@iit.ac.lk>
2025-04-29 19:44:10 +0530

    user model updated

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,15 @@
 package model;
 
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String passwordHash;
 }
